715b9db459a923b09961132aa3fe350c56bc5990
Karin Morais <karin.andrade@dnc.group>
2022-05-24 20:41:50 -0300

    Adding the discounts rules

--- src/Discounts/MoreThan500ReaisDiscount.php
+++ src/Discounts/MoreThan500ReaisDiscount.php
@@ -2,7 +2,17 @@
 
 namespace DesignPattern\Discounts;
 
-class MoreThan500Reais
+use DesignPattern\Budget;
+class MoreThan500ReaisDiscount extends Discount
 {
+
+  public function calculateDiscounts(Budget $budget): float
+  {
+    if($budget->value > 500) {
+      return $budget->value * 0.05;
+    }
+
+    return $this->nextDiscount->calculateDiscounts($budget);
+  }
 
 }
--- src/Discounts/MoreThan5ItemsDiscount.php
+++ src/Discounts/MoreThan5ItemsDiscount.php
@@ -2,7 +2,18 @@
 
 namespace DesignPattern\Discounts;
 
-class MoreThan5ItemsDiscount
+use DesignPattern\Budget;
+
+class MoreThan5ItemsDiscount extends Discount
 {
+
+  public function calculateDiscounts(Budget $budget): float
+  {
+    if($budget->itemsQuantity > 5) {
+      return $budget->value * 0.1;
+    }
+
+    return $this->nextDiscount->calculateDiscounts($budget);
+  }
 
 }
