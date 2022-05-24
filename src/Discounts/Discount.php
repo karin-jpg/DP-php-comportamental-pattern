ddd7c47f5b03bfe11254af8cc4d669625ebb3906
Karin Morais <karin.andrade@dnc.group>
2022-05-24 20:41:22 -0300

    Adding the rules to the class

--- src/Discounts/Discount.php
+++ src/Discounts/Discount.php
@@ -1,8 +1,18 @@
 <?php
 
 namespace DesignPattern\Discounts;
+use DesignPattern\Budget;
 
 abstract class Discount
 {
+
+  protected Discount $nextDiscount;
+
+  public function __construct(?Discount $nextDiscount)
+  {
+    $this->$nextDiscount = $nextDiscount;
+  }
+
+  abstract public function calculateDiscounts(Budget $budget): float;
 
 }
