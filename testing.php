5450a9d0839f8cc5bbde75a81de7a4ec21e82502
Karin Morais <karin.andrade@dnc.group>
2022-05-24 20:00:40 -0300

    Testing the discount calculator

--- testing.php
+++ testing.php
@@ -1,15 +1,29 @@
 <?php
 
+//Strategy implementation
 use DesignPattern\TaxCalculator;
 use DesignPattern\Budget;
 use DesignPattern\Taxes\Icms;
 use DesignPattern\Taxes\Iss;
 
+//Chain of responsability implementation
+use DesignPattern\DiscountCalculator;
+
 require 'vendor/autoload.php';
 
+/* Tax calculator testing - Strategy implementation
 $taxCalculator = new TaxCalculator();
 $budget = new Budget();
 
 $budget->value = 100;
 
 echo $taxCalculator->calculateTax($budget, new Icms());
+*/
+
+
+$budget = new Budget();
+$budget->value = 600;
+$budget->itemsQuantity = 5;
+
+$discountCalculator = new DiscountCalculator();
+echo $discountCalculator->calculateDiscounts($budget);
