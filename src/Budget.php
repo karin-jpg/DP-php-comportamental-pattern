70191411fc7eebe94543ab0b60c4d502bc520ee4
Karin Morais <karin.andrade@dnc.group>
2022-05-25 20:01:42 -0300

    Added new extra discount based on state of the budget

--- src/Budget.php
+++ src/Budget.php
@@ -2,9 +2,30 @@
 
 namespace DesignPattern;
 
+use DomainException;
+
 class Budget 
 {
   public int $itemsQuantity;
   public float $value;
+  public string $currentState;
 
+
+  public function applyExtraDiscount() 
+  {
+    $this->valor -= $this->calculateExtraDiscount();
+  }
+
+  public function calculateExtraDiscount(): float 
+  {
+    if ($this->currentState == "ON_APPROVAL") {
+      return $this->valor * 0.05;
+    }
+
+    if ($this->currentState == "APPROVED") {
+      return $this->valor * 0.02;
+    }
+
+    throw new \DomainException("Reproved and finalized budgets cannot receive discounts");
+  }
 }
