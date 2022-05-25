dd138f136075d2e05b0df49b68c60835eaa07dc5
Karin Morais <karin.andrade@dnc.group>
2022-05-25 20:34:29 -0300

    Added testing cases of 2 design patterns

--- testing.php
+++ testing.php
@@ -8,6 +8,8 @@ use DesignPattern\Taxes\Iss;
 
 //Chain of responsability implementation
 use DesignPattern\DiscountCalculator;
+use DesignPattern\Taxes\Icpp;
+use DesignPattern\Taxes\Ikcv;
 
 require 'vendor/autoload.php';
 
@@ -20,10 +22,25 @@ $budget->value = 100;
 echo $taxCalculator->calculateTax($budget, new Icms());
 */
 
+/* Discount calculator testing - Chain of responsability implementation
+// $budget = new Budget();
+// $budget->value = 600;
+// $budget->itemsQuantity = 5;
 
-$budget = new Budget();
-$budget->value = 600;
-$budget->itemsQuantity = 5;
+// $discountCalculator = new DiscountCalculator();
+// echo $discountCalculator->calculateDiscounts($budget);
+*/
 
-$discountCalculator = new DiscountCalculator();
-echo $discountCalculator->calculateDiscounts($budget);
+
+/* New Tax with 2 aliquots testing - Template method implementation
+// $budget = new Budget();
+// $budget->value = 700;
+// $budget->itemsQuantity = 2;
+
+//Ex 1
+// $taxWith2AliquotsCalculator = new Icpp();
+
+//Ex 2
+// $taxWith2AliquotsCalculator = new Ikcv();
+// echo $taxWith2AliquotsCalculator->calculateTax($budget);
+*/
