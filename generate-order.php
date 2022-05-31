45ed52c20a2ffd7f055a12dc5eb0e157f6325a96
Karin Morais <karin.andrade@dnc.group>
2022-05-30 21:37:20 -0300

    Added calling for handler actions

--- generate-order.php
+++ generate-order.php
@@ -3,6 +3,8 @@
 require 'vendor/autoload.php';
 
 use DesignPattern\{Budget, GenerateOrder, GenerateOrderHandler, Order};
+use DesignPattern\ActionsOnCreateOrder\CreateOrderLog;
+use DesignPattern\ActionsOnCreateOrder\CreateOrderOnDB;
 
 $budgetValue = $argv[1];
 $itemsQuantity = $argv[2];
@@ -10,4 +12,6 @@ $clientName = $argv[3];
 
 $generateOrder = new GenerateOrder($budgetValue, $itemsQuantity, $clientName);
 $generateOrderHandler = new GenerateOrderHandler();
+$generateOrderHandler->AddActionAfterCreatingOrder(new CreateOrderOnDB);
+$generateOrderHandler->AddActionAfterCreatingOrder(new CreateOrderLog);
 $generateOrderHandler->execute($generateOrder);
